858364982fea7bb3bd0eca2694c8187397a005e8
Isaac Poole <isaacpoolemusic@gmail.com>
2021-04-01 19:28:21 -0400

    adds get example

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
+import prisma from "../../prisma/client";
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default async (req, res) => {
+  if (req.method === "GET") {
+    res
+      .status(200)
+      .json(await prisma.user.findUnique({ where: { name: "isfopo" } }));
+  }
+};
