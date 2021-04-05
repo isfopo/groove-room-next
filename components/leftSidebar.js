477a9a949dbe7a52d89303ef09d9c14af30a6d5a
Isaac Poole <isaacpoolemusic@gmail.com>
2021-04-05 16:49:44 -0400

    adds icons

--- components/leftSidebar.js
+++ components/leftSidebar.js
@@ -1,11 +1,26 @@
 import React from "react";
 
+import Add from "../public/icons/Add";
+import Join from "../public/icons/Group";
+import List from "../public/icons/List";
+
 export const LeftSidebar = (props) => {
-  const { rooms } = props;
+  const { rooms, handleSetRoom } = props;
+  console.log(rooms);
+
+  const handleClick = () => {
+    console.log("click");
+  };
 
   return (
     <div>
-      <h1>leftSidebar</h1>
+      <p>left sidebar</p>
+      {rooms.map((room) => {
+        return <p onClick={() => handleSetRoom(room)}>{room.name}</p>;
+      })}
+      <Add onClick={() => handleClick()} />
+      <Join onClick={() => handleClick()} />
+      <List />
     </div>
   );
 };
