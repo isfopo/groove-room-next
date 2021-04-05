ef6c00617ff4037936da56851a48ccd75473d4c2
Isaac Poole <isaacpoolemusic@gmail.com>
2021-04-05 12:32:55 -0400

    sets up nest-auth with spotify

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+const options = {
+  providers: [
+    Providers.Spotify({
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.display_name,
+          email: profile.email,
+          image: profile.images?.[0]?.url,
+        };
+      },
+    }),
+  ],
+};
+
+export default (req, res) => NextAuth(req, res, options);
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import styles from "../styles/App.module.css";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function App() {
+  const [session, loading] = useSession();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -9,7 +12,21 @@ export default function App() {
       </Head>
 
       <main className={styles.main}>
-        <h1>hi</h1>
+        {!session && (
+          <>
+            <h1>You are not logged in</h1>
+            <button onClick={signIn}>Sign In</button>
+          </>
+        )}
+
+        {session && (
+          <>
+            {" "}
+            {console.log(session)}
+            <h1>You are signed in as {session.user.email}</h1>
+            <button onClick={signOut}>Sign Out</button>
+          </>
+        )}
       </main>
 
       <footer className={styles.footer}></footer>
