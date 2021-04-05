9ce050ea7f57df828d0725adc19e71ba64f2d123
Isaac Poole <isaacpoolemusic@gmail.com>
2021-04-05 14:51:30 -0400

    integrates leftsidebar

--- components/home.js
+++ components/home.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import { LeftSidebar } from "./leftSidebar";
+
 export const Home = (props) => {
   const { session } = props;
   const [user, setUser] = useState({});
@@ -15,11 +17,13 @@ export const Home = (props) => {
       }),
     });
     const json = await res.json();
+    console.log(json);
     setUser(json);
   }, []);
 
   return (
     <div>
+      <LeftSidebar rooms={user.rooms} />
       <h1>{user.name}</h1>
       <p>{user.email}</p>
       <img src={user.avatar} />
