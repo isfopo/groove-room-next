0395fda7a2f6bc03403676a3f6fd6d420eaea195
Isaac Poole <isaacpoolemusic@gmail.com>
2021-04-05 14:36:24 -0400

    gets user from database

--- components/home.js
+++ components/home.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const Home = (props) => {
   const { session } = props;
-  const { user } = session;
-  console.log(user);
+  const [user, setUser] = useState({});
+
+  useEffect(async () => {
+    const res = await fetch("http://localhost:3000/api/user", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        ...session.user,
+      }),
+    });
+    const json = await res.json();
+    setUser(json);
+  }, []);
+
   return (
     <div>
       <h1>{user.name}</h1>
-      <img src={user.picture} />
+      <p>{user.email}</p>
+      <img src={user.avatar} />
     </div>
   );
 };
