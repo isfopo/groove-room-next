da738b2b55be7ed9709ae255173bba1daba8975a
Isaac Poole <isaacpoolemusic@gmail.com>
2021-04-06 10:17:01 -0400

    gets messages for each user in the room

--- components/user.js
+++ components/user.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const User = (props) => {
-  const { user } = props;
+  const { user, room } = props;
+  const [messages, setMessages] = useState();
 
+  useEffect(async () => {
+    const res = await fetch(
+      `http://localhost:3000/api/message?user=${user.id}&room=${room.id}`
+    );
+    setMessages(await res.json());
+  }, [room]);
+  // TODO: if avatar is null, show default avatar
   return (
     <div>
       <h1>{user.name}</h1>
       <img src={user.avatar} />
+      {messages?.map((message, key) => (
+        <p key={key}>{message.content}</p>
+      ))}
     </div>
   );
 };
